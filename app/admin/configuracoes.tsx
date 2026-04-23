52b68c39f885c7ff8ef702930280f1351e881a68
joaopedropc7 <joaopedropcunha7@gmail.com>
2026-04-23 16:34:24 -0300

    novo

--- app/admin/configuracoes.tsx
+++ app/admin/configuracoes.tsx
@@ -1,10 +1,5 @@
-import { AdminShell } from '@/features/admin/components/AdminShell';
 import { AdminConfiguracoesView } from '@/features/admin/screens/AdminConfiguracoesView';
 
 export default function AdminConfiguracoesPage() {
-  return (
-    <AdminShell>
-      <AdminConfiguracoesView />
-    </AdminShell>
-  );
+  return <AdminConfiguracoesView />;
 }
